83d134997165371b8d52699381403b7b9c35fb50
valentynf <valentynf@wix.com>
2025-06-03 14:40:09 +0300

    minor cleanup, adding letterbox comp

---
+++ src/components/LetterBox/LetterBox.tsx
@@ -0,0 +1,12 @@
+import styles from './LetterBox.module.css';
+
+function LetterBox() {
+  return (
+    <>
+      <div className={styles['letterbox-container']}></div>
+    </>
+  );
+}
+
+export default LetterBox;
+// This component renders a letterbox styled div using CSS modules.
