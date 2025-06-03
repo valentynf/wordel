3143b898902f28fb155413ba04bcf1956672039d
valentynf <valentynf@wix.com>
2025-06-03 17:04:52 +0300

    added adding input to the row, not removing tho :))

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import Row from '../Row/Row';
 import styles from './Game.module.css';
 
 function Game() {
+  const [currentGuess, setCurrentGuess] = useState<string>('');
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      console.log(e.key);
+      if (e.key == ' ') console.log('SPACE');
+      if (/^[A-Z]$/i.test(e.key)) handleAddLetter(e.key);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  const handleAddLetter = (letter: string) => {
+    setCurrentGuess((prev) => (prev.length < 5 ? prev + letter : prev));
+  };
+
   return (
     <div className={styles['game-container']}>
-      <Row wordInput="sraka" />
+      <Row wordInput={currentGuess} />
     </div>
   );
 }
