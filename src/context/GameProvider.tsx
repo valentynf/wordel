3f1ea00ab0b3ed1953374b4a6dc93c9e63879a19
valentynf <valentynf@wix.com>
2025-06-04 23:01:39 +0300

    minor improvements

--- src/context/GameProvider.tsx
+++ src/context/GameProvider.tsx
@@ -1,8 +1,97 @@
 import { useReducer, type ReactNode } from 'react';
-import { reducer, initialState } from '../state/gameReducer';
 import GameContext from './GameContext';
+import type {
+  BoxStatus,
+  GameState,
+  GameStateReducerAction,
+} from '../types/appTypes';
 
-function GameProvider({ children }: { children: ReactNode }) {
+const answer = 'SHARK';
+
+const initialState: GameState = {
+  view: 'start',
+  rows: Array(6)
+    .fill(0)
+    .map(() => ({
+      letters: [],
+      statuses: Array(5).fill('default' as BoxStatus),
+    })),
+  currentRow: 0,
+};
+
+function reducer(prevState: GameState, action: GameStateReducerAction) {
+  switch (action.type) {
+    case 'add-letter': {
+      const { letter } = action.payload;
+      const { currentRow, rows } = prevState;
+
+      if (rows[currentRow].letters.length >= 5) return prevState;
+
+      return {
+        ...prevState,
+        rows: rows.map((row, i) =>
+          i === currentRow ? { ...row, letters: [...row.letters, letter] } : row
+        ),
+      };
+    }
+    case 'remove-letter': {
+      const { currentRow, rows } = prevState;
+
+      if (rows[currentRow].letters.length < 1) return prevState;
+
+      return {
+        ...prevState,
+        rows: rows.map((row, i) =>
+          i === currentRow
+            ? {
+                ...row,
+                letters: row.letters.slice(0, row.letters.length - 1),
+              }
+            : row
+        ),
+      };
+    }
+    case 'submit-guess': {
+      const { currentRow, rows } = prevState;
+
+      const lettersAnswer: string[] = answer.split('');
+      const lettersGuess: string[] = rows[currentRow].letters;
+
+      const boxStatuses: BoxStatus[] = lettersGuess
+        .map((letter) =>
+          answer.includes(letter.toUpperCase()) ? 'present' : 'wrong'
+        )
+        .map((status, i) =>
+          lettersGuess[i].toUpperCase() === lettersAnswer[i].toUpperCase()
+            ? 'correct'
+            : status
+        );
+
+      return {
+        ...prevState,
+        rows: rows.map((row, i) =>
+          i === currentRow
+            ? {
+                ...row,
+                statuses: boxStatuses,
+              }
+            : row
+        ),
+      };
+    }
+    case 'next-row': {
+      return { ...prevState, currentRow: prevState.currentRow + 1 };
+    }
+    case 'set-view': {
+      const { view } = action.payload;
+      return { ...prevState, view: view };
+    }
+    default:
+      return prevState;
+  }
+}
+
+export function GameProvider({ children }: { children: ReactNode }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
@@ -11,5 +100,3 @@ function GameProvider({ children }: { children: ReactNode }) {
     </GameContext.Provider>
   );
 }
-
-export default GameProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import GameProvider from './context/GameProvider.tsx';
+import { GameProvider } from './context/GameProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
