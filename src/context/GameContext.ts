6490476a567cfe37317e8f152bf5f68a2b554c13
valentynf <valentynf@wix.com>
2025-06-05 12:47:17 +0300

    now definitely finalized context files structure

--- src/context/GameContext.ts
+++ src/context/GameContext.ts
@@ -1,5 +1,92 @@
 import { createContext, type Dispatch } from 'react';
-import type { GameState, GameStateReducerAction } from '../types/appTypes';
+import type {
+  BoxStatus,
+  GameState,
+  GameStateReducerAction,
+} from '../types/appTypes';
+
+const answer = 'SHARK';
+
+export const initialState: GameState = {
+  view: 'start',
+  rows: Array(6)
+    .fill(0)
+    .map(() => ({
+      letters: [],
+      statuses: Array(5).fill('default' as BoxStatus),
+    })),
+  currentRow: 0,
+};
+
+export function reducer(
+  prevState: GameState,
+  action: GameStateReducerAction
+): GameState {
+  switch (action.type) {
+    case 'add-letter': {
+      const { letter } = action.payload;
+      const { currentRow, rows } = prevState;
+
+      if (rows[currentRow].letters.length >= 5) return prevState;
+
+      return {
+        ...prevState,
+        rows: rows.map((row, i) =>
+          i === currentRow ? { ...row, letters: [...row.letters, letter] } : row
+        ),
+      };
+    }
+
+    case 'remove-letter': {
+      const { currentRow, rows } = prevState;
+      if (rows[currentRow].letters.length < 1) return prevState;
+
+      return {
+        ...prevState,
+        rows: rows.map((row, i) =>
+          i === currentRow
+            ? {
+                ...row,
+                letters: row.letters.slice(0, row.letters.length - 1),
+              }
+            : row
+        ),
+      };
+    }
+
+    case 'submit-guess': {
+      const { currentRow, rows } = prevState;
+      const lettersAnswer = answer.split('');
+      const lettersGuess = rows[currentRow].letters;
+
+      const boxStatuses: BoxStatus[] = lettersGuess
+        .map((letter) =>
+          answer.includes(letter.toUpperCase()) ? 'present' : 'wrong'
+        )
+        .map((status, i) =>
+          lettersGuess[i].toUpperCase() === lettersAnswer[i].toUpperCase()
+            ? 'correct'
+            : status
+        );
+
+      return {
+        ...prevState,
+        rows: rows.map((row, i) =>
+          i === currentRow ? { ...row, statuses: boxStatuses } : row
+        ),
+      };
+    }
+
+    case 'next-row':
+      return { ...prevState, currentRow: prevState.currentRow + 1 };
+
+    case 'set-view':
+      return { ...prevState, view: action.payload.view };
+
+    default:
+      return prevState;
+  }
+}
 
 const GameContext = createContext<
   { state: GameState; dispatch: Dispatch<GameStateReducerAction> } | undefined
