b2d4e1c0fa78d54fac57522c8db58549a6ac7b01
valentynf <valentynf@wix.com>
2025-06-11 00:30:40 +0300

    moved input handling logic to a separate hook, immplemented blocking input when word is loading or being checked

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,49 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styles from './Game.module.css';
 import Row from './Row/Row';
 import { useGameContext } from '../../hooks/useGameContext';
 import useRandomWord from '../../hooks/useRandomWord';
-import { checkIfRealWord } from '../../api/wordsApi';
+import useGameInputHandler from '../../hooks/useGameInputHandler';
 
 function Game() {
   const { state: gameData, dispatch } = useGameContext();
-  const { rows, currentRow } = gameData;
   const { word: answer, isLoading: isGettingWord } = useRandomWord();
-  const [isCheckingWord, setIsCheckingWord] = useState<boolean>(false);
+
+  useGameInputHandler(isGettingWord);
 
   useEffect(() => {
     dispatch({ type: 'set-answer', payload: { answer: answer } });
   }, [answer, dispatch]);
-
-  useEffect(() => {
-    const handleInput = async (input: string) => {
-      if (isGettingWord || isCheckingWord) return;
-      const currentGuess = rows[currentRow].letters.join('');
-      if (input === 'Enter' && currentGuess.length === 5) {
-        try {
-          setIsCheckingWord(true);
-          const isReal = await checkIfRealWord(currentGuess);
-          if (!isReal) return;
-          dispatch({ type: 'submit-guess' });
-          if (currentRow < 5) dispatch({ type: 'next-row' });
-        } catch (err) {
-          console.log(err);
-        } finally {
-          setIsCheckingWord(false);
-        }
-      }
-      if (/^[A-Z]$/i.test(input))
-        dispatch({ type: 'add-letter', payload: { letter: input } });
-      if (input === 'Backspace') dispatch({ type: 'remove-letter' });
-    };
-    const handleKeyDown = (e: KeyboardEvent) => handleInput(e.key);
-
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [currentRow, dispatch, rows, isCheckingWord, isGettingWord]);
 
   return (
     <div className={styles['game-container']}>
---
+++ src/hooks/useGameInputHandler.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+import { checkIfRealWord } from '../api/wordsApi';
+import { useGameContext } from './useGameContext';
+
+function useGameInputHandler(isGettingWord: boolean) {
+  const { state: gameData, dispatch } = useGameContext();
+  const { rows, currentRow } = gameData;
+  const isGettingWordRef = useRef(isGettingWord);
+  const [isCheckingWord, setIsCheckingWord] = useState<boolean>(false);
+
+  useEffect(() => {
+    isGettingWordRef.current = isGettingWord;
+  }, [isGettingWord]);
+
+  useEffect(() => {
+    const handleInput = async (input: string): Promise<void> => {
+      if (isGettingWordRef.current || isCheckingWord) return;
+      const currentGuess = rows[currentRow].letters.join('');
+      if (input === 'Enter' && currentGuess.length === 5) {
+        try {
+          setIsCheckingWord(true);
+          const isReal = await checkIfRealWord(currentGuess);
+          if (!isReal) return;
+          dispatch({ type: 'submit-guess' });
+          if (currentRow < 5) dispatch({ type: 'next-row' });
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setIsCheckingWord(false);
+        }
+      }
+      if (/^[A-Z]$/i.test(input))
+        dispatch({ type: 'add-letter', payload: { letter: input } });
+      if (input === 'Backspace') dispatch({ type: 'remove-letter' });
+    };
+    const handleKeyDown = (e: KeyboardEvent) => handleInput(e.key);
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentRow, dispatch, rows, isCheckingWord]);
+}
+
+export default useGameInputHandler;
