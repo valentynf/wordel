08027cbbadd8046c1076efb8253450650a6061c7
valentynf <valentynf@wix.com>
2025-06-11 01:08:50 +0300

    updated rules for random words from api

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const GET_WORD_API_URL =
-  'https://api.wordnik.com/v4/words.json/randomWord?minLength=5&maxLength=5';
+  'https://api.wordnik.com/v4/words.json/randomWord?minLength=5&maxLength=5&hasDictionaryDef=true&includePartOfSpeech=noun,verb,adjective&excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive&minDictionaryCount=1';
